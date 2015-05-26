1be406951fb3dd3e19680358a6b95a900462646c
MikeMeliz <mikemel@hotmail.gr>
2015-05-26 06:12:44 +0300

    Project's Files

---
+++ EasyCaptureForm.Designer.vb
@@ -0,0 +1,120 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class EasyCaptureForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(EasyCaptureForm))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lstVideoSources = New System.Windows.Forms.ListBox()
+        Me.PhotoPictureBox = New System.Windows.Forms.PictureBox()
+        Me.btnCapturePhoto = New System.Windows.Forms.Button()
+        Me.btnStopCamera = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PhotoPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.PictureBox1.Location = New System.Drawing.Point(2, 45)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(593, 420)
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(280, 14)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(79, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Video Sources:"
+        '
+        'lstVideoSources
+        '
+        Me.lstVideoSources.FormattingEnabled = True
+        Me.lstVideoSources.Location = New System.Drawing.Point(365, 9)
+        Me.lstVideoSources.Name = "lstVideoSources"
+        Me.lstVideoSources.Size = New System.Drawing.Size(230, 30)
+        Me.lstVideoSources.TabIndex = 5
+        '
+        'PhotoPictureBox
+        '
+        Me.PhotoPictureBox.Location = New System.Drawing.Point(505, 386)
+        Me.PhotoPictureBox.Name = "PhotoPictureBox"
+        Me.PhotoPictureBox.Size = New System.Drawing.Size(85, 68)
+        Me.PhotoPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PhotoPictureBox.TabIndex = 18
+        Me.PhotoPictureBox.TabStop = False
+        '
+        'btnCapturePhoto
+        '
+        Me.btnCapturePhoto.Location = New System.Drawing.Point(9, 2)
+        Me.btnCapturePhoto.Name = "btnCapturePhoto"
+        Me.btnCapturePhoto.Size = New System.Drawing.Size(161, 37)
+        Me.btnCapturePhoto.TabIndex = 19
+        Me.btnCapturePhoto.Text = "Capture Photo"
+        Me.btnCapturePhoto.UseVisualStyleBackColor = True
+        '
+        'btnStopCamera
+        '
+        Me.btnStopCamera.Location = New System.Drawing.Point(176, 2)
+        Me.btnStopCamera.Name = "btnStopCamera"
+        Me.btnStopCamera.Size = New System.Drawing.Size(98, 37)
+        Me.btnStopCamera.TabIndex = 20
+        Me.btnStopCamera.Text = "Close Camera"
+        Me.btnStopCamera.UseVisualStyleBackColor = True
+        '
+        'EasyCaptureForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(600, 468)
+        Me.Controls.Add(Me.btnStopCamera)
+        Me.Controls.Add(Me.btnCapturePhoto)
+        Me.Controls.Add(Me.PhotoPictureBox)
+        Me.Controls.Add(Me.lstVideoSources)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "EasyCaptureForm"
+        Me.Text = "EasyCapture!"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PhotoPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents lstVideoSources As System.Windows.Forms.ListBox
+    Friend WithEvents PhotoPictureBox As System.Windows.Forms.PictureBox
+    Friend WithEvents btnCapturePhoto As System.Windows.Forms.Button
+    Friend WithEvents btnStopCamera As System.Windows.Forms.Button
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+
+End Class
---
+++ EasyCaptureForm.vb
@@ -0,0 +1,148 @@
+﻿'---Εισαγωγή Βιβλιοθηκών Windows---
+Imports System.Runtime.InteropServices
+
+Public Class EasyCaptureForm
+    '---Δήλωση σταθερών για εντολές Cameras---
+    Const WM_CAP_START = &H400S
+    Const WS_CHILD = &H40000000
+    Const WS_VISIBLE = &H10000000
+
+    Const WM_CAP_DRIVER_CONNECT = WM_CAP_START + 10
+    Const WM_CAP_DRIVER_DISCONNECT = WM_CAP_START + 11
+    Const WM_CAP_EDIT_COPY = WM_CAP_START + 30
+    Const WM_CAP_SEQUENCE = WM_CAP_START + 62
+    Const WM_CAP_FILE_SAVEAS = WM_CAP_START + 23
+
+    Const WM_CAP_SET_SCALE = WM_CAP_START + 53
+    Const WM_CAP_SET_PREVIEWRATE = WM_CAP_START + 52
+    Const WM_CAP_SET_PREVIEW = WM_CAP_START + 50
+
+    Const SWP_NOMOVE = &H2S
+    Const SWP_NOSIZE = 1
+    Const SWP_NOZORDER = &H4S
+    Const HWND_BOTTOM = 1
+
+    '---Δήλωση Μεταβλητής για αλλαγή ονόματος αποθηκευμένης εικόνας---
+    Dim x As Integer
+
+
+    '--The capGetDriverDescription function retrieves the version description of the capture driver--
+    Declare Function capGetDriverDescriptionA Lib "avicap32.dll" _
+       (ByVal wDriverIndex As Short, _
+        ByVal lpszName As String, ByVal cbName As Integer, ByVal lpszVer As String, _
+        ByVal cbVer As Integer) As Boolean
+
+    '--The capCreateCaptureWindow function creates a capture window--
+    Declare Function capCreateCaptureWindowA Lib "avicap32.dll" _
+       (ByVal lpszWindowName As String, ByVal dwStyle As Integer, _
+        ByVal x As Integer, ByVal y As Integer, ByVal nWidth As Integer, _
+        ByVal nHeight As Short, ByVal hWnd As Integer, _
+        ByVal nID As Integer) As Integer
+
+    '--This function sends the specified message to a window or windows--
+    Declare Function SendMessage Lib "user32" Alias "SendMessageA" _
+       (ByVal hwnd As Integer, ByVal Msg As Integer, ByVal wParam As Integer, _
+       <MarshalAs(UnmanagedType.AsAny)> ByVal lParam As Object) As Integer
+
+    '--Sets the position of the window relative to the screen buffer--
+    Declare Function SetWindowPos Lib "user32" Alias "SetWindowPos" _
+       (ByVal hwnd As Integer, _
+        ByVal hWndInsertAfter As Integer, ByVal x As Integer, ByVal y As Integer, _
+        ByVal cx As Integer, ByVal cy As Integer, ByVal wFlags As Integer) As Integer
+
+    '--This function destroys the specified window--
+    Declare Function DestroyWindow Lib "user32" (ByVal hndw As Integer) As Boolean
+
+    Dim VideoSource As Integer
+    Dim hWnd As Integer
+
+    '--disconnect from video source---
+    Private Sub StopPreviewWindow()
+        SendMessage(hWnd, WM_CAP_DRIVER_DISCONNECT, VideoSource, 0)
+        DestroyWindow(hWnd)
+    End Sub
+
+    '---list all the various video sources---
+    Private Sub ListVideoSources()
+        Dim DriverName As String = Space(80)
+        Dim DriverVersion As String = Space(80)
+        For i As Integer = 0 To 9
+            If capGetDriverDescriptionA(i, DriverName, 80, DriverVersion, 80) Then
+                lstVideoSources.Items.Add(DriverName.Trim)
+            End If
+        Next
+    End Sub
+
+    '---save the image---
+    Private Sub CaptureImage()
+        Dim data As IDataObject
+        Dim bmap As Image
+
+        '---copy the image to the clipboard---
+        SendMessage(hWnd, WM_CAP_EDIT_COPY, 0, 0)
+
+        '---retrieve the image from clipboard and convert it 
+        ' to the bitmap format
+        data = Clipboard.GetDataObject()
+        If data.GetDataPresent(GetType(System.Drawing.Bitmap)) Then
+            bmap = _
+               CType(data.GetData(GetType(System.Drawing.Bitmap)),  _
+               Image)
+            PhotoPictureBox.Image = bmap
+
+        End If
+    End Sub
+
+
+    '---preview the selected video source---
+    Private Sub PreviewVideo(ByVal pbCtrl As PictureBox)
+        hWnd = capCreateCaptureWindowA(VideoSource, WS_VISIBLE Or WS_CHILD, 0, 0, 0, _
+            0, pbCtrl.Handle.ToInt32, 0)
+        If SendMessage(hWnd, WM_CAP_DRIVER_CONNECT, VideoSource, 0) Then
+            '---set the preview scale---
+            SendMessage(hWnd, WM_CAP_SET_SCALE, True, 0)
+            '---set the preview rate (ms)---
+            SendMessage(hWnd, WM_CAP_SET_PREVIEWRATE, 30, 0)
+            '---start previewing the image---
+            SendMessage(hWnd, WM_CAP_SET_PREVIEW, True, 0)
+            '---resize window to fit in PictureBox control---
+            SetWindowPos(hWnd, HWND_BOTTOM, 0, 0, _
+               pbCtrl.Width, pbCtrl.Height, _
+               SWP_NOMOVE Or SWP_NOZORDER)
+        Else
+            '--error connecting to video source---
+            DestroyWindow(hWnd)
+        End If
+    End Sub
+
+
+    Private Sub lstVideoSources_SelectedIndexChanged( _
+       ByVal sender As System.Object, ByVal e As System.EventArgs) _
+       Handles lstVideoSources.SelectedIndexChanged
+
+        '---stop video in case it is on---
+        StopPreviewWindow()
+        '---check which video source is selected---
+        VideoSource = lstVideoSources.SelectedIndex
+        '---preview the selected video source
+        PreviewVideo(PictureBox1)
+    End Sub
+
+    Private Sub btnStopCamera_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnStopCamera.Click
+        StopPreviewWindow()
+    End Sub
+
+    Private Sub btnCapturePhoto_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCapturePhoto.Click
+        Dim bt As Image
+        x = x + 1
+        CaptureImage()
+        SendMessage(hWnd, WM_CAP_EDIT_COPY, 1, 0)
+        bt = Clipboard.GetImage
+        bt.Save(Application.StartupPath & "\Picture" & x & ".jpg", Imaging.ImageFormat.Jpeg)
+    End Sub
+
+    Private Sub EasyCaptureForm_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        '---list all the video sources---
+        ListVideoSources()
+    End Sub
+End Class
